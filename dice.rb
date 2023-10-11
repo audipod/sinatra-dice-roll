00287a1cca34647a4f951a6521756ab910b984c3
audipod <146144639+audipod@users.noreply.github.com>
2023-10-11 00:22:12 +0000

    added target_blank

--- dice.rb
+++ dice.rb
@@ -2,17 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "<h1> Dice Rolls </h1>
+  "<body> 
+  <h1> Dice Rolls </h1>
   <p> Congratulations. You've stumbled upon the ultimate dice-rolling website. </p>
 
   <h2> Quick Links </h2>
 
   <ul>
-    <li> <a href = https://shiny-umbrella-4j75q7p7j7q7f5jrp-4567.app.github.dev/dice/2/6> 2d6 </a>
-    <li> <a href = https://shiny-umbrella-4j75q7p7j7q7f5jrp-4567.app.github.dev/dice/2/10> 2d10 </a>
-    <li> <a href = https://shiny-umbrella-4j75q7p7j7q7f5jrp-4567.app.github.dev/dice/1/20> 1d20 </a>
-    <li> <a href = https://shiny-umbrella-4j75q7p7j7q7f5jrp-4567.app.github.dev/dice/5/4> 5d4 </a>
-  </ul>"
+    <li> <a href = https://shiny-umbrella-4j75q7p7j7q7f5jrp-4567.app.github.dev/dice/2/6 target=_blank> 2d6 </a>
+    <li> <a href = https://shiny-umbrella-4j75q7p7j7q7f5jrp-4567.app.github.dev/dice/2/10 target=_blank> 2d10 </a>
+    <li> <a href = https://shiny-umbrella-4j75q7p7j7q7f5jrp-4567.app.github.dev/dice/1/20 target=_blank> 1d20 </a>
+    <li> <a href = https://shiny-umbrella-4j75q7p7j7q7f5jrp-4567.app.github.dev/dice/5/4 target=_blank> 5d4 </a>
+  </ul>
+  </body>"
   
 
 
